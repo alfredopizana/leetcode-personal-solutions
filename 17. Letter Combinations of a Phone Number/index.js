d46218361d1894ff6f52dc55dad55184bf8c0075
AlfredoPizana <pizana.alfredo@gmail.com>
2021-08-16 23:17:22 -0500

    Basic implementatio of Letter Combinations

--- 17. Letter Combinations of a Phone Number/index.js
+++ 17. Letter Combinations of a Phone Number/index.js
@@ -29,3 +29,63 @@ Constraints:
 digits[i] is a digit in the range ['2', '9'].
 
 */
+
+
+/*
+We need to define which letters are contained on every number
+e.g.
+2 = [a,b,c]
+3 = [d,e,f]
+4 = [g,h,i]
+5 = [j,k,l]
+*/
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+ /**
+ * @param {string} digits
+ * @return {string[]}
+ */
+function Dictionary(){
+    this.digits = []
+    this.digits[2] = ["a","b","c"]
+    this.digits[3] = ["d","e","f"]
+    this.digits[4] = ["g","h","i"]
+    this.digits[5] = ["j","k","l"]
+    this.digits[6] = ["m","n","o"]
+    this.digits[7] = ["p","q","r","s"]
+    this.digits[8] = ["t","u","v"]
+    this.digits[9] = ["w","x","y","z"]
+}
+function Node(value){
+    this.data = value;
+    this.children = [];
+    this.dictionary = new Dictionary();
+    this.generateChildren = (numbers)=>{
+        let currentNumber = parseInt(numbers.charAt(0))
+        let pendingNumbers = numbers.substring(1)
+        let combinations = this.dictionary.digits[currentNumber];
+        for(let index=0; index < combinations.length; index++){
+            let child =  new Node(combinations[index]);
+            if(pendingNumbers.length > 0)
+                child.generateChildren(pendingNumbers);
+            this.children.push(child)
+        }
+    }
+    this.getRecursiveCombinations = ()=>{
+        let combinations = []
+        for(let index = 0; children.lenght;index++){
+            
+            combinations.push(children[index].getRecursiveCombinations())
+        }
+        return combinations
+    }
+}
+let node = new Node("")
+node.generateChildren("23")
+var letterCombinations = function(digits) {
+    
+};
