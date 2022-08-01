dfe566725733e2fa5072665170171eb3b7140c1c
AlfredoPizana <ap@simplea.com>
2022-08-01 10:01:13 -0500

    1628 Algorithm solution

--- 1628. Design an Expression Tree With Evaluate Function/AddTwoNumbers/index.js
+++ 1628. Design an Expression Tree With Evaluate Function/AddTwoNumbers/index.js
@@ -31,9 +31,12 @@ class TreeNode extends Node {
     evaluate(){
       if(!isNaN(this.root)) 
         return Number(this.root)
-
+      // root = * 
+      // left = 3 : Node
+      // right = 4
       const left = this.left.evaluate()
       const right = this.right.evaluate()
+      // mathOperators[this.root] = funcion (a,b) => return a+b
       return mathOperators[this.root](left,right);
     }
 }
@@ -53,17 +56,22 @@ class TreeBuilder {
      * @param {string[]} s
      * @return {Node}
      */
+
+    // s = ["3","4","+","2","*","7","/"]
     buildTree(postfix) {
       let stack = []
       for(let index = 0;index < postfix.length;index++){
         let right;
         let left;
+        // 0
         let rootValue = postfix[index]
+        // + * - 
         if(isNaN(rootValue))
         {
-          right = stack.pop();
-          left =  stack.pop();
+          right = stack.pop(); // 4
+          left =  stack.pop(); // 3
         }
+        // + // left = 3 // right = 4
         let root = new TreeNode(rootValue,left,right)
         stack.push(root)
       }
@@ -80,10 +88,10 @@ class TreeBuilder {
  */
 
 var obj = new TreeBuilder();
-var expTree = obj.buildTree(["3","4","+","2","*","7","/"]);
+var expTree = obj.buildTree(["3","4","+","2","*","7","/"]); //2
 var ans = expTree.evaluate();
 console.log(ans)
 
-var expTree = obj.buildTree(["4","5","2","7","+","-","*"]);
+var expTree = obj.buildTree(["4","5","2","7","+","-","*"]); // -16
 var ans = expTree.evaluate();
 console.log(ans)
